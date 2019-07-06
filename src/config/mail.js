6319e6c132093a9da756babde10b8ba05138eb15
Camila  Satie <camila@MacBook-Pro-de-Camila.local>
2019-07-05 23:34:11 -0300

    Appointments com notificações de cancelamento

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '8faca197a94f13',
+    pass: '32482d5ebb678a',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreplay@gobarber.com>',
+  },
+};
